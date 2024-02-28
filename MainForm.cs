8c2850707363f697661d7c22f5b00b9d1bc12a26
vlasov2198 <vlasov2198@gmail.com>
2024-02-28 17:26:22 +0300

    Добавьте файлы проекта.

---
+++ MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace drivingschool
+{
+    public partial class MainForm : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["drivingschooldb"].ConnectionString);
+            
+            sqlConnection.Open();
+
+        }
+    }
+}
---
+++ createdatabase.sql
@@ -0,0 +1,32 @@
+﻿-- Создание таблицы "Students"
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY IDENTITY(1,1),
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Age INT,
+    Phone NVARCHAR(20)
+);
+
+-- Создание таблицы "Locations"
+CREATE TABLE Locations (
+    LocationID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100)
+);
+
+-- Создание таблицы "LessonTypes"
+CREATE TABLE LessonTypes (
+    LessonTypeID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100)
+);
+
+-- Создание таблицы "Schedule"
+CREATE TABLE Schedule (
+    ScheduleID INT PRIMARY KEY IDENTITY(1,1),
+    LessonDate DATE,
+    StartTime TIME,
+    EndTime TIME,
+    StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+    LocationID INT FOREIGN KEY REFERENCES Locations(LocationID),
+    LessonTypeID INT FOREIGN KEY REFERENCES LessonTypes(LessonTypeID),
+    Grade INT -- Оценка за занятие
+);
